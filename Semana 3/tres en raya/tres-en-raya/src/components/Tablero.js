da5f308fb5ca616106cd0cc9a90128497dd735ec
Calvin <95377583+Kaal-I-Vayn@users.noreply.github.com>
2024-01-17 14:52:30 +0000

    Semana 3 tres en ray y lista de notas

--- Semana 3/tres en raya/tres-en-raya/src/components/Tablero.js
+++ Semana 3/tres en raya/tres-en-raya/src/components/Tablero.js
@@ -1,48 +1,30 @@
+import React from 'react';
+import Cuadro from './Cuadro';
 
-import Cuadro from "./Cuadro";
-import { useState } from "react";
-function Tablero(){
-    const [cuadros, setCuadros] = useState(Array(9).fill(null));
-    const [jugador, setJugador]=useState("O")
-    const click = (i) => {
-        console.log("Posicion: "+i);
-        console.log("Turno: "+jugador);
-        const cuadrosTemp=[...cuadros];
+function Tablero({ cuadros, onClick }) {
+    function renderizarCuadro(i) {
+        return (
+            <Cuadro
+                valor={cuadros[i]}
+                funcion={() => onClick(i)}
+            />
+        );
+    }
 
-        
-        if(cuadrosTemp[i]===null){
-            cuadrosTemp[i]=jugador;
-            setCuadros(cuadrosTemp);
-            console.log(cuadrosTemp);
-            setJugador(jugador==="X"? "O":"X")
-            
-        }
-        else{                        
-            console.log("no paso nada aqui")            
-        }
-        
-    
-    };
-    return(
-        <div className="juego">
-            <h1>Siguiente jugador: {jugador}</h1>
-            <div className="tablero">            
-            
-            <Cuadro valor={cuadros[0]} funcion={() => click(0)}/>
-            <Cuadro valor={cuadros[1]} funcion={() => click(1)}/>
-            <Cuadro valor={cuadros[2]} funcion={() => click(2)}/>
-            <Cuadro valor={cuadros[3]} funcion={() => click(3)}/>
-            <Cuadro valor={cuadros[4]} funcion={() => click(4)}/>
-            <Cuadro valor={cuadros[5]} funcion={() => click(5)}/>
-            <Cuadro valor={cuadros[6]} funcion={() => click(6)}/>
-            <Cuadro valor={cuadros[7]} funcion={() => click(7)}/>
-            <Cuadro valor={cuadros[8]} funcion={() => click(8)}/>            
+    return (
+        <div>
+            <div className="tablero">
+                {renderizarCuadro(0)}
+                {renderizarCuadro(1)}
+                {renderizarCuadro(2)}
+                {renderizarCuadro(3)}
+                {renderizarCuadro(4)}
+                {renderizarCuadro(5)}
+                {renderizarCuadro(6)}
+                {renderizarCuadro(7)}
+                {renderizarCuadro(8)}
             </div>
-            <div className="boton-reiniciar">                
-            </div>
-
-            
         </div>
-    )
+    );
 }
 export default Tablero;
