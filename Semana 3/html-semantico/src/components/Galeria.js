bca9d9adb9ac32c14bffb51add8045c9230b2d1c
Calvin <95377583+Kaal-I-Vayn@users.noreply.github.com>
2024-01-19 15:55:47 +0000

    routers 19 de enero

--- Semana 3/html-semantico/src/components/Galeria.js
+++ Semana 3/html-semantico/src/components/Galeria.js
@@ -3,6 +3,7 @@ import '../styles/Galeria.css'
 export default function Galeria(){
     return(
         <div className='galeria'>
+            <h1> <img src={require(`../images/imagen1.png`)}/>Pepito</h1>
             <div className='contenedor-image'>
                 <img src={require(`../images/imagen1.png`)}/>
                 <img src={require(`../images/imagen1.png`)}/>
